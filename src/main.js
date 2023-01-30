03ba378cdb183d8406994f4acf87da58286c6501
Coien-rr <972054003@qq.com>
2023-01-30 18:37:16 +0800

    🔧 build(store): Introduce Pinia

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import router from './router'
+import piniaStore from './store'
 import '~/style/style.css'
 import App from './App.vue'
 
 createApp(App)
+  .use(piniaStore)
   .use(router)
   .mount('#app')
